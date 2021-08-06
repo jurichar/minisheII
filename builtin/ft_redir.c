227916a21e0eb7d5cc0c53e4cb17d99022a880ca
Julien RICHARD <jurichar@e1r2p16.42.fr>
2021-08-06 17:51:17 +0200

    ll

--- builtin/ft_redir.c
+++ builtin/ft_redir.c
@@ -2,45 +2,47 @@
 
 void	ft_redir(t_cmd_lst *lst, t_env_lst *envlst)
 {
-	int status;
-	int inout = lst->redir->redir;
-	char *inoutput = lst->redir->arg;
-	char BUF[128];
+	int		status;
+	int		inout;
+	pid_t	pid;
+	char	*inoutput;
+	char	BUF[128];
 
-	pid_t pid = fork();
+	pid = fork();
+	inoutput = lst->redir->arg;
+	inout = lst->redir->redir;
 	if (pid < 0)
 		perror("fork");
 	else if (pid == 0)
 	{
-		if (inout == 1) // <
+		if (inout == 1)
 		{
 			lst->fd[0] = open(inoutput, O_RDONLY);
 			dup2(lst->fd[0], 0);
 			close(lst->fd[0]);
 		}
-		else if (inout == 2) // >
+		else if (inout == 2)
 		{
 			lst->fd[1] = open(inoutput, O_CREAT | O_RDWR | O_TRUNC, 0644);
 			dup2(lst->fd[1], 1);
 			close(lst->fd[1]);
 		}		
-		else if (inout == 3) // >>
+		else if (inout == 3)
 		{
 			lst->fd[1] = open(inoutput, O_CREAT | O_RDWR | O_APPEND, 0644);
 			dup2(lst->fd[1], 1);
 			close(lst->fd[1]);
 		}
-		// else if (inout == 4) // <<
-		// {
-		// 	fd0 = open(inoutput, O_RDONLY);
-		// 	dup2(fd0, 0);
-		// 	read(fd0, BUF, ft_strlen(inoutput));
-		// 	printf (">>> %s <<\n", BUF);
-		// 	// while (ft_strcmp(BUF, inoutput) != 0)
-		// 	// 	wait(NULL);
-		// 	close(fd0);
-		// }
-    }
+		else if (inout == 4)
+		{
+			lst->fd[0] = open(inoutput, O_RDONLY);
+			dup2(lst->fd[0], 0);
+			read(lst->fd[0], BUF, ft_strlen(inoutput));
+			while (ft_strcmp(BUF, inoutput) != 0)
+				wait(NULL);
+			close(lst->fd[0]);
+		}
+	}
 	else
 	{
 		waitpid(pid, &status, 0);

9722bb997188a630dcdff340fe2e2946de6a3c68
lebourre <bourretleo@hotmail.fr>
2021-11-03 15:48:40 +0100

    je crois c bon

--- .ccls-cache/@home@user42@cursus@minishell@git/srcs@env_var_manager.c
+++ .ccls-cache/@home@user42@cursus@minishell@git/srcs@env_var_manager.c
@@ -104,32 +104,3 @@ char	*insert_env_var(char *str, int i, t_env_lst *env, int squote)
 	free(copy);
 	return (str);
 }
-
-char	*find_env_var(char *str, t_env_lst *env, int i, int quote)
-{
-	char	*s;
-
-	s = ft_strdup(str);
-	while (s[++i])
-	{
-		quote = double_quote_status(quote, i, s);
-		if (quote == 0 && s[i] == '\'')
-		{
-			quote = get_to_next_quote(s, i);
-			if (s[quote])
-				i = quote;
-			continue ;
-		}
-		if (quote == 0 && is_sep(s[i]))
-			break ;
-		else if (s[i] == '$')
-		{
-			s = insert_env_var(s, i, env, 0);
-			if (!s)
-				return (NULL);
-			i = -1;
-		}
-	}
-	free(str);
-	return (s);
-}
